521b886ce4410e2a6aef468cd6cbf0aaaabbea0c
laurisseau <reso0208@gmail.com>
2023-03-01 03:26:31 -0500

    hkg

--- backend/schedules.js
+++ backend/schedules.js
@@ -1,3 +1,7 @@
-module.exports.test = function(){
+export const test1 = function(){
         console.log('This is the Schedule called...');
+}
+
+export default function test2(){
+    console.log('This is the 2nd Schedule called...');
 }
