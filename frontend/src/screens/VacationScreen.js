7cd0b86cb1cc95246cec0f0a4dd9fa2d0fd020e9
laurisseau Joseph <reso0208@gmail.com>
2023-06-14 23:50:17 -0400

    6/14/23

--- frontend/src/screens/VacationScreen.js
+++ frontend/src/screens/VacationScreen.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import { useState} from 'react';
 import { DateRangePicker } from 'react-date-range';
+import Button from "react-bootstrap/Button";
 import 'react-date-range/dist/styles.css'; // Import the default styles
 import 'react-date-range/dist/theme/default.css';
-
+import NavbarComp from "../components/NavbarComp";
 
 export default function VacationScreen() {
     const [selectedDates, setSelectedDates] = useState([
@@ -16,21 +17,27 @@ export default function VacationScreen() {
     const handleDateChange = (ranges) => {
       setSelectedDates([ranges.selection]);
     };
-  
+
+
     return (
       <div>
+        <NavbarComp color="dark" />
+      <div className='vacation-container mt-5 mb-5'>
         <DateRangePicker
           ranges={selectedDates}
           onChange={handleDateChange}
           months={2}
-          direction='horizantal'
+          direction='horizontal'
+          className='vacation-box'
         />
         {selectedDates.length > 0 && (
-          <div>
-            Selected start date: {selectedDates[0].startDate.toDateString()} <br />
-            Selected end date: {selectedDates[0].endDate.toDateString()}
+          <div className='text-center vacation-text'>
+             From: {selectedDates[0].startDate.toDateString()}<br /> 
+             To: {selectedDates[0].endDate.toDateString()}<br />
+            <Button className='mt-1 ps-3 pe-3'>Submit</Button>
           </div>
         )}
+      </div>
       </div>
     );
   };
